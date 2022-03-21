a495d59f6b4ab1775a81de4d611ea8e6cb078135
camidangot <101379301+camidangot@users.noreply.github.com>
2022-03-21 18:15:43 -0400

    Update Sums.m
    added a graph of sex distribution

--- Sums.m
+++ Sums.m
@@ -29,14 +29,48 @@ sum_other = sum(dummyrace_other)
 
 %bar graph of the number of each race that was pulled over 
 h = figure
+subplot(2,1,1)
 x = [1:5];
 y = [sum_asian, sum_black, sum_hispanic, sum_white, sum_other];
 b = bar(x, y ,0.75);
 b.FaceColor = '#FFB6C1'
 title('Race Distribution of Cars Pulled Over');
 xlabel('Races');
-ylabel('Number of People Searched');
+ylabel('Number of Cars Pulled Over');
 set(gca,'xticklabel',{'Asian','Black','White','Hispanic','Other'})
+
+%% SEX DUMMY VARIABLES
+
+%make dummy columns (Aisha, 3/21 5-5:30PM)
+sex_columns = data(:,38);
+sex_array = table2array(sex_columns);
+n_sex_array = nominal(sex_array);
+D_S = dummyvar(n_sex_array);
+disp(D_S);
+
+%make dummy variables (Aisha, 3/21 5-5:30PM)
+dummysex_female = D_S(:,1);
+dummysex_male = D_S(:,2) ;
+Traffic_Violations_Table = [Traffic_Violations_Table array2table(dummysex_male) array2table(dummysex_female)];
+disp(Traffic_Violations_Table)
+
+%sums for sex
+sum_female = sum(dummysex_female);
+sum_male = sum(dummysex_male);
+
+%bar graph of the number of each race that was pulled over 
+subplot(2,1,2)
+x = [1:2];
+y = [sum_female, sum_male];
+g = bar(x, y ,0.75);
+g.FaceColor = '#CBC3E3'
+title('Sex Distribution of Cars Pulled Over');
+xlabel('Sex');
+ylabel('Number of Cars Pulled Over');
+set(gca,'xticklabel',{'Female','Male'})
+
+
+
 
 
 
