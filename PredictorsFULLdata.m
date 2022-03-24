766f42fa8f7cd213886a568e7f65f68e24429f21
camidangot <101379301+camidangot@users.noreply.github.com>
2022-03-23 21:24:21 -0400

    Update PredictorsFULLdata.m
    Added a bunch of different predictor combinations like race with time and race and gender

--- PredictorsFULLdata.m
+++ PredictorsFULLdata.m
@@ -3,13 +3,13 @@ data = readtable(filename,'PreserveVariableNames',true);
 
 %% RACE DUMMY VARIABLES
 
-%make dummy columns (Aisha, 3/21 2PM-4PM)
+%make dummy columns 
 race_columns = data(:,37);
 race_array = table2array(race_columns);
 n_race_array = nominal(race_array);
 D_R = dummyvar(n_race_array);
 
-%make dummy variables (Aisha, 3/21 4PM-5PM)
+%make dummy variables 
 dummyrace_asian = D_R(:,1);
 dummyrace_black = D_R(:,2) ;
 dummyrace_hispanic = D_R(:,3) ;
@@ -19,27 +19,17 @@ Traffic_Violations_Table = [data array2table(dummyrace_asian) array2table(dummyr
 
 %% SEX DUMMY VARIABLES
 
-%make dummy columns (Aisha, 3/21 5-5:30PM)
+%make dummy columns 
 sex_columns = data(:,38);
 sex_array = table2array(sex_columns);
 n_sex_array = nominal(sex_array);
 D_S = dummyvar(n_sex_array);
 
-%make dummy variables (Aisha, 3/21 5-5:30PM)
+%make dummy variables 
 dummysex_female = D_S(:,1);
 dummysex_male = D_S(:,2) ;
 Traffic_Violations_Table = [Traffic_Violations_Table array2table(dummysex_male) array2table(dummysex_female)];
 
-% %dummy citation v. warning
-% citation_warning_columns = data(:,22);
-% citation_warning_array = table2array(citation_warning_columns);
-% n_citation_warning_array = nominal(citation_warning_array);
-% D_CW = dummyvar(n_citation_warning_array);
-% 
-% %dummy search variables
-% dummysearch_citation = D_CW(:,1);
-% dummysearch_warning = D_CW(:,2) ;
-% Traffic_Violations_Table = [Traffic_Violations_Table array2table(dummysearch_citation) array2table(dummysearch_warning)];
 
 
 %% TIME DUMMY VARIABLES
@@ -72,19 +62,7 @@ dummyviolation_ESERO = D_V(:,3);
 Traffic_Violations_Table = [data array2table(dummyviolation_citation) array2table(dummyviolation_warning) array2table(dummyviolation_ESERO)];
 
 
-
-% %seeing reelationships
-% data = data(~isnan('data.dummyrace_asian' ==0),:);
-% data = data(~isnan('dummysearch_citation' ==0),:);
-% data.dummysearch_citation = categorical(data.dummysearch_citation);
-% predictor1 = data.(dummyrace_asian);
-% predictor2 = data.(dummysearch_citation);
-% predictor_vector = [predictor1 predictor2];
-% [M, dec,stats] = mnrfit(predictor1, predictor2)
-
 %RREDICTORS FOR RACE 
-%data = data(any('dummyrace_asian'),:);
-%data = data(any('dummysearch_citation'),:);
 predictor1 = dummyrace_asian;
 predictor2 = dummyrace_black;
 predictor3 = dummyrace_white;
@@ -122,16 +100,71 @@ dummyviolation_citation = categorical(dummyviolation_citation);
 predictor_vector = [predictor2];
 [M_day, dec_day,stats_day] = mnrfit(predictor_vector, dummyviolation_citation)
 
+%COMBINING DIFFERENT PREDICTORS:
 
-% %trying to see how asian predicts getting a citation but it did not work
-% predictor1 = data.(dummyrace_asian);
-% [M, dev,stats] = mnrfit(dummyrace_asian, dummysearch_citation, 'Model','hierarchical')
-% [M, dev,stats] = mnrfit(dummyrace_black, dummysearch_citation, 'Model','hierarchical')
-% [M, dev,stats] = mnrfit(dummyrace_white, dummysearch_citation, 'Model','hierarchical')
-% [M, dev,stats] = mnrfit(dummyrace_hispanic, dummysearch_citation, 'Model','hierarchical')
-% %[M, dev,stats] = mnrfit(dummyrace_other, dummysearch_citation, 'Model','hierarchical')
+%RACE AND TIME:
+%PREDICTORS FOR ASIAN AND TIME(DAY) ON CITATIONS
+predictor1 = dummyrace_asian;
+predictor2 = dummytime_daytime;
+predictor3 = dummytime_nighttime;
+dummyviolation_citation = categorical(dummyviolation_citation);
+predictor_vector = [predictor1, predictor2];
+[M_asday, dec_asday,stats_asday] = mnrfit(predictor_vector, dummyviolation_citation)
 
-%another way we were trying with isabella
-%load data
-%const = ones(height(DataTable),1);
-%DataTable = aadvars(DataTable, Const, Before = 1);
+%RREDICTORS FOR BLACK AND TIME(DAY) ON CITATIONS
+predictor1 = dummyrace_black;
+predictor2 = dummytime_daytime;
+predictor3 = dummytime_nighttime;
+dummyviolation_citation = categorical(dummyviolation_citation);
+predictor_vector = [predictor1, predictor2];
+[M_blday, dec_blday,stats_blday] = mnrfit(predictor_vector, dummyviolation_citation)
+
+%RREDICTORS FOR WHITE AND TIME(DAY) ON CITATIONS
+predictor1 = dummyrace_white;
+predictor2 = dummytime_daytime;
+predictor3 = dummytime_nighttime;
+dummyviolation_citation = categorical(dummyviolation_citation);
+predictor_vector = [predictor1, predictor2];
+[M_whday, dec_whday,stats_whday] = mnrfit(predictor_vector, dummyviolation_citation)
+
+%RREDICTORS FOR HISPANIC AND TIME(DAY) ON CITATIONS
+predictor1 = dummyrace_hispanic;
+predictor2 = dummytime_daytime;
+predictor3 = dummytime_nighttime;
+dummyviolation_citation = categorical(dummyviolation_citation);
+predictor_vector = [predictor1, predictor2];
+[M_hispday, dec_hispday,stats_hispday] = mnrfit(predictor_vector, dummyviolation_citation)
+
+%RACE AND GENDER
+%PREDICTORS FOR ASIAN AND MALE ON CITATIONS
+predictor1 = dummyrace_asian;
+predictor2 = dummysex_male;
+predictor3 = dummysex_female;
+dummyviolation_citation = categorical(dummyviolation_citation);
+predictor_vector = [predictor1, predictor2];
+[M_asmale, dec_asmale,stats_asmale] = mnrfit(predictor_vector, dummyviolation_citation)
+
+%PREDICTORS FOR BLACK AND MALE ON CITATIONS
+predictor1 = dummyrace_black;
+predictor2 = dummysex_male;
+predictor3 = dummysex_female;
+dummyviolation_citation = categorical(dummyviolation_citation);
+predictor_vector = [predictor1, predictor2];
+[M_blmale, dec_blmale,stats_blmale] = mnrfit(predictor_vector, dummyviolation_citation)
+
+%PREDICTORS FOR WHITE AND MALE ON CITATIONS
+predictor1 = dummyrace_white;
+predictor2 = dummysex_male;
+predictor3 = dummysex_female;
+dummyviolation_citation = categorical(dummyviolation_citation);
+predictor_vector = [predictor1, predictor2];
+[M_wmale, dec_wmale,stats_wmale] = mnrfit(predictor_vector, dummyviolation_citation)
+
+%PREDICTORS FOR HISPANIC AND MALE ON CITATIONS
+predictor1 = dummyrace_hispanic;
+predictor2 = dummysex_male;
+predictor3 = dummysex_female;
+dummyviolation_citation = categorical(dummyviolation_citation);
+predictor_vector = [predictor1, predictor2];
+[M_hispmale, dec_hispmale,stats_hispmale] = mnrfit(predictor_vector, dummyviolation_citation)
+
